5266af0a2d2d0710901fcaeb3f09a46a7df0b6b3
kgetman <kgetmanskii@gmail.com>
2022-06-13 18:21:37 +0200

    Converted BeachListViewModel to Kotlin

---
+++ app/src/main/java/com/getman/varnabeach/lifecycle/BeachListViewModel.kt
@@ -0,0 +1,22 @@
+package com.getman.varnabeach.lifecycle
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.getman.varnabeach.data.Repository
+import com.getman.varnabeach.room.Beach
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BeachListViewModel @Inject constructor(
+    repository: Repository
+) : ViewModel() {
+    val allBeaches: LiveData<List<Beach>> = repository.allBeaches
+    private lateinit var chosenBeachMutable: MutableLiveData<Beach>
+    val chosenBeach: LiveData<Beach> = chosenBeachMutable
+
+    fun chooseBeach(selectedBeach: Beach) {
+        chosenBeachMutable.value = selectedBeach
+    }
+}
