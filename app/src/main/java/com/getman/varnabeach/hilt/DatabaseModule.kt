d395591c60dc957d6de0060772d6f91937f5d5d8
Kirill Getmanskii <57256881+xameYeoh@users.noreply.github.com>
2022-06-14 22:55:54 +0200

    Moved provide db methods to DatabaseModule.kt

--- app/src/main/java/com/getman/varnabeach/hilt/DatabaseModule.kt
+++ app/src/main/java/com/getman/varnabeach/hilt/DatabaseModule.kt
@@ -2,11 +2,8 @@ package com.getman.varnabeach.hilt
 
 import android.content.Context
 import androidx.room.Room
-import com.getman.varnabeach.VarnaBeachApplication
 import com.getman.varnabeach.room.BeachDAO
 import com.getman.varnabeach.room.BeachDatabase
-import com.getman.varnabeach.util.ErrorHandler
-import com.getman.varnabeach.util.ErrorToastDisplayer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,9 +13,13 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule {
+object DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideBeachDatabase(@ApplicationContext context: Context): BeachDatabase {
+        return BeachDatabase.getInstance(context)
+    }
 
     @Provides
-    fun provideErrorHandler(@ApplicationContext context: Context): ErrorHandler =
-        ErrorToastDisplayer(context)
+    fun provideBeachDAO(db: BeachDatabase): BeachDAO = db.beachDao()
 }
